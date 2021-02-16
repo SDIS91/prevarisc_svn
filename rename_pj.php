58345d2402ca2a84dee025efa82d7fc7521d7b76
Taoufik Messaoud <tmessaoud@sdis91.fr>
2021-02-16 15:53:07 +0000

    rajout des logs dans le script
    git-svn-id: https://srv-dev-deb.intra.sdis91.fr/repos/prevarisc/trunk@143 2d3c187e-36cd-11e6-ac83-4d600e4afb08

--- rename_pj.php
+++ rename_pj.php
@@ -1,21 +1,32 @@
 <?php
 
 ini_set("display_errors", 1);
-ini_set('max_execution_time', 3600); // 1h
+ini_set('max_execution_time', 3600); // 1h 
 
 $path_abs = '/home/www/html/prevarisc/public';
 $path_log = $path_abs.'/log';
 $path = '/home/www/html/prevarisc/public/tmp_PJ';
 $destination = '/home/www/html/prevarisc/public/propre_pj';
 
+//*****************
+// les logs
+//*****************
+$start = date("d-m-Y H:i:s");
+$logfile = $path_log.'/rename_log.log';
+$logmode = 'a';
+$lfp = fopen($logfile, $logmode);
+fwrite($lfp, "\nStarted at $start\n");
+$time_deb = time(true);
+
+//*************
+//BASE
+//**************
 $user ="root";
 $host ="localhost";
 $password ="lmdpqt6";
 $database ="geside_recup";
 $connect = new mysqli($host, $user, $password, $database);
 
-$doc = array();
-$fichiers = array();
 $tab_extension = array('pdf','PDF','doc','DOC','xls','XLS','csv','CSV','pps','PPS','ppt','PPT','jpg','JPG','odt','ODT','zip','ZIP','png','PNG','rtf','RTF','docx','DOCX');
 
 $files = scandir($path);
@@ -28,20 +39,31 @@ foreach($files as $file){
 		
 		if($fichier[1] == $tab_extension[$j]){
 			$nompj = $file;
+			
+			//requete qui permet de recuperer les PJ dans la table de fichiersdisque
 			$sql = $connect -> query("SELECT * FROM fichiersdisque WHERE NOM_FICHIER = '$nompj'");
 			$data = $sql -> fetch_row();
 			
-			//Array ( [0] => 1 [1] => 1.pdf [2] => .pdf [3] => 2011-12-08 [4] => 51672 [5] => 32344 [6] => ) 
+			// print_r($data) => Array ( [0] => 1 [1] => 1.pdf [2] => .pdf [3] => 2011-12-08 [4] => 51672 [5] => 32344 [6] => ) 
 
 			if(isset($data) && count($data)>0 && $data[4] > 0){
 				//je renomme les fichiers
 				$newname = $data[4].'.'.strtolower($tab_extension[$j]);
 				rename($path.'/'.$file, $destination.'/'.$newname);
+				fwrite($lfp, "success rename : $nompj ====> $newname \n\n");
 				echo '- '.$file.'  ==>  renomme en =======> '.$newname;
 				echo '<br>';
+			}else{
+				fwrite($lfp, "KO rename : $nompj\n\n ");
 			}
 		}
 	}
 }
+
+$time_fin = time(true);
+$exec_time = $time_fin - $time_deb;
+$time = "Temps d'excution : $exec_time s\n";
+fwrite($lfp, $time);
+fclose($lfp);
 
 ?>

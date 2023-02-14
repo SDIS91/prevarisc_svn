1dce847a8e56c5b05139963fddc764ca53d2ece7
Webmaster91 <GSIC-Etudes_Dev@sdis91.fr>
2023-02-14 16:36:38 +0100

    Delete sql directory

--- sql/migrations/2.5.35-ajout_ref_courrier_dossier.sql
+++
@@ -1,3 +0,0 @@
-set names 'utf8';
-
-ALTER TABLE `dossier` ADD `REFCOURRIER_DOSSIER` varchar(15) DEFAULT NULL;
--- sql/privileges.sql
+++
@@ -1,2 +0,0 @@
-/* Bloc "Etudes en attente de traitement par type de commission" en remplacement du bloc "Dossiers suivis non verrouillés" dans le Dashboard.*/
-UPDATE `privileges` SET `name` = 'view_commissions_etudes', `text` = 'Voir les études en attente de traitement' WHERE `privileges`.`id_privilege` = 108;
